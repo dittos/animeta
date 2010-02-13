420ddea3077fe9c3727064c1abfd33f07615f388
ditto <ditto@ditto-laptop>
2010-02-13 15:22:16 +0900

    refactoring (factored out history template)

---
+++ animeta/record/templatetags/history.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from django import template
+
+register = template.Library()
+
+@register.inclusion_tag("history.html")
+def show_history(history, options="user,work"):
+	options = options.split(',')
+	return {
+		'history': history,
+		'show_user': 'user' in options,
+		'show_work': 'work' in options,
+	}
