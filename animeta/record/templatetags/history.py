b70bd2751e2e63c708bf6f066d42ae139a12ec8c
Kim Taeho <dittos@gmail.com>
2011-01-26 14:58:49 +0900

    record가 없을 때 죽는 문제 수정 (레거시 ㅠ.ㅠ)

--- animeta/record/templatetags/history.py
+++ animeta/record/templatetags/history.py
@@ -7,10 +7,14 @@ register = template.Library()
 @register.inclusion_tag("history.html", takes_context=True)
 def show_history(context, history, options="user,work"):
 	options = options.split(',')
+	try:
+		can_delete = 'delete' in options and history.user == context['user'] and history.record.history_set.count() > 1 # XXX: N+1 query problem
+	except:
+		can_delete = False
 	return {
 		'user': context['user'],
 		'history': history,
 		'show_user': 'user' in options,
 		'show_work': 'work' in options,
-		'can_delete': 'delete' in options and history.user == context['user'] and history.record.history_set.count() > 1 # XXX: N+1 query problem
+		'can_delete': can_delete
 	}
