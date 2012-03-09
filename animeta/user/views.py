a3aefc7fb0ef3b4c881b23901b88f760a7be5645
Taeho Kim <dittos@gmail.com>
2012-03-09 23:30:37 +0900

    이번 주, 지난 달 추가

--- animeta/user/views.py
+++ animeta/user/views.py
@@ -63,11 +63,15 @@ def shortcut(request, username):
 def _date_header(date):
     # 오늘/어제/그저께/그끄저께/이번 주/지난 주/이번 달/지난 달/YYYY-MM
     today = datetime.date.today()
-    dt = lambda days: today - datetime.timedelta(days=days)
+    dt = lambda **kwargs: today - datetime.timedelta(**kwargs)
     if date == today: return u'오늘'
     elif date == dt(days=1): return u'어제'
     elif date == dt(days=2): return u'그저께'
     elif date == dt(days=3): return u'그끄저께'
+    elif date.isocalendar()[:2] == today.isocalendar()[:2]:
+        return u'이번 주'
+    elif date.isocalendar()[:2] == dt(weeks=1).isocalendar()[:2]:
+        return u'지난 주'
     elif date.year == today.year and date.month == today.month:
         return u'이번 달'
     else:
