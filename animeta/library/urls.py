2e53ff8794c9e10eec21e8e7d88423a279991673
Taeho Kim <dittos@gmail.com>
2012-08-19 17:16:53 +0900

    Remove incomplete "library" UI implementation

--- animeta/library/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('library.views',
-    url(r'^$', 'library'),
-    url(r'^(?P<id>\d+)$', 'item_detail'),
-    url(r'^(?P<id>\d+)/update$', 'update_item'),
-)
--- animeta/urls.py
+++ animeta/urls.py
@@ -14,7 +14,6 @@ urlpatterns = patterns('',
     (r'^api/', include('animeta.api.urls')),
 
     (r'^library/', login_required(animeta.user.views.library)),
-    (r'^users/(?P<username>[A-Za-z0-9_]+)/library/', include('animeta.library.urls')),
     (r'^users/(?P<username>[A-Za-z0-9_]+)/', include('animeta.user.urls')),
     (r'^records/', include('animeta.record.urls')),
     (r'^works/', include('animeta.work.urls')),
