70e7275052568345b517e3e7de6fe7fce0e89014
Taeho Kim <dittos@gmail.com>
2012-09-12 22:58:55 +0900

    Update jQuery and re-organize JS code

--- animeta/static/common.js
+++ animeta/static/common.js
@@ -20,4 +20,31 @@ $(function () {
         return false
     })
 */
+    
+    $('#suffix').after('<a href="#" id="plus-one"><img src="' + window.STATIC_URL + 'plus.gif" alt="+1" /></a>')
+    function determine_suffix() {
+        var status = $('#id_status');
+        if (status.length == 0) return;
+        if (status.val().match(/^(|.*\d)$/))
+            $('#suffix').show()
+        else
+            $('#suffix').hide()
+    }
+    determine_suffix()
+    $('#id_status').change(determine_suffix).keyup(determine_suffix)
+    $('#plus-one').click(function(event) {
+        var $field = $('#id_status')
+        var val = $field.val()
+        var matches = val.match(/(\d+)[^\d]*$/)
+        if (matches)
+        {
+            var digits = matches[1].length
+            var add1 = (parseInt(matches[1], 10) + 1).toString()
+            if (add1.length < digits)
+                for (var i = 0; i < digits - add1.length; i++)
+                    add1 = '0' + add1
+            $field.val(val.replace(/(\d+)([^\d]*)$/, add1 + '$2'))
+        }
+        return false
+    })
 })
