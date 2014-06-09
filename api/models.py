d36df67f487ce7893ce69b70652abafa8e83835e
Taeho Kim <dittos@gmail.com>
2014-06-09 21:22:20 +0900

    Extend access token expiry on access

--- api/models.py
+++ api/models.py
@@ -6,18 +6,21 @@ from django.core.cache import cache
 from django.conf import settings
 from django.contrib.auth.models import User
 
+DAY = 60 * 60 * 24
+DEFAULT_TTL = 14 * DAY
+
 def generate_session_token(user):
     # Generate token
     token = ':'.join([settings.SECRET_KEY, str(user.username), os.urandom(10)])
     token = hashlib.sha256(token).hexdigest()
 
     # Store token
-    days = 60 * 60 * 24
-    cache.set('apisession:%s' % token, user.username, 14 * days)
-
+    cache.set('apisession:%s' % token, user.username, DEFAULT_TTL)
     return token
 
 def get_user_from_token(token):
-    username = cache.get('apisession:%s' % token)
+    key = 'apisession:%s' % token
+    username = cache.get(key)
     if not username: return None
+    cache.set(key, username, DEFAULT_TTL)
     return User.objects.get(username=username)
