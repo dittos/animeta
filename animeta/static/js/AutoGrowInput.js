b56367b4304f08b3839fdfb163f6814c0be35e95
Taeho Kim <dittos@gmail.com>
2014-08-15 17:17:28 +0900

    Reactify post composer

---
+++ animeta/static/js/AutoGrowInput.js
@@ -0,0 +1,22 @@
+/** @jsx React.DOM */
+
+var AutoGrowInput = React.createClass({
+    getInitialState() {
+        return {size: this.props.minSize};
+    },
+
+    componentDidMount() {
+        this.setState({size: this.getDOMNode().value.length});
+    },
+
+    render() {
+        var width = Math.max(this.props.minSize, Math.min(this.props.maxSize, this.state.size)) + 'em';
+        return this.transferPropsTo(<input onChange={this.handleChange} style={{width: width}} />);
+    },
+
+    handleChange(event) {
+        this.setState({size: event.target.value.length});
+    }
+});
+
+module.exports = AutoGrowInput;
--- animeta/static/js/library.react.js
+++ animeta/static/js/library.react.js
@@ -1,6 +1,7 @@
 /** @jsx React.DOM */
 
 var BaseStore = require('./BaseStore');
+var AutoGrowInput = require('./AutoGrowInput');
 
 class RecordStore extends BaseStore {
     constructor(title, categoryId) {
@@ -145,6 +146,73 @@ var HeaderView = React.createClass({
     }
 });
 
+var StatusInputView = React.createClass({
+    getInitialState() {
+        return {showSuffix: true};
+    },
+
+    render() {
+        return <span>
+            {this.transferPropsTo(<AutoGrowInput minSize={3} maxSize={10} onChange={this._updateSuffix} ref="input" />)}
+            {this.state.showSuffix ? '화' : null}
+            <span className="plus-one" style={{cursor: 'pointer'}} onClick={this.handlePlusOne}>
+                <img src="/static/plus.gif" alt="+1" />
+            </span>
+        </span>;
+    },
+
+    componentDidMount() {
+        this._updateSuffix();
+    },
+
+    _updateSuffix() {
+        var input = this.refs.input.getDOMNode();
+        this.setState({showSuffix: input.value.match(/^(|.*\d)$/)});
+    },
+
+    handlePlusOne() {
+        var input = this.refs.input.getDOMNode();
+        var newValue = plusOne(input.value);
+        input.value = newValue;
+    }
+});
+
+var PostComposerView = React.createClass({
+    render() {
+        var currentStatus;
+        if (this.props.currentStatus) {
+            currentStatus = <span className="progress-current">{this.props.currentStatus} &rarr; </span>;
+        }
+        return <form className="record-detail-update" method="post" data-connected-services={this.props.connectedServices}>
+            <div className="progress">
+                <select name="status_type" defaultValue={this.props.initialStatusType}>
+                    <option value="watching">보는 중</option>
+                    <option value="finished">완료</option>
+                    <option value="suspended">중단</option>
+                    <option value="interested">볼 예정</option>
+                </select>
+                {' @ '}
+                {currentStatus}
+                <StatusInputView id="id_status" name="status"
+                    defaultValue={plusOne(this.props.currentStatus)} />
+            </div>
+            <textarea name="comment" rows={3} cols={30} autoFocus />
+            <div className="actions">
+                {'공유: '}
+                <input type="checkbox" id="id_publish_twitter" name="publish_twitter" />
+                <label htmlFor="id_publish_twitter">트위터</label>
+                <input type="checkbox" id="id_publish_facebook" name="publish_facebook" />
+                <label htmlFor="id_publish_facebook">페이스북</label>
+                <button type="submit">기록 추가</button>
+            </div>
+        </form>;
+    },
+
+    componentDidMount() {
+        initServiceToggles($(this.getDOMNode()));
+    }
+});
+
 function getRecordStoreState() {
     return {
         title: recordStore.getTitle(),
@@ -166,13 +234,17 @@ var AppView = React.createClass({
     },
 
     render() {
-        return (
+        return <div>
             <HeaderView
                 recordId={this.props.recordId}
                 title={this.state.title}
                 categoryId={this.state.categoryId}
                 categoryList={this.props.categoryList} />
-        );
+            <PostComposerView
+                currentStatus={this.props.status}
+                initialStatusType={this.props.statusType}
+                connectedServices={this.props.connectedServices} />
+        </div>;
     }
 });
 
