1df0d8a0c3516a72b4949ddb50c5853515b47278
Ditto Kim <dittos@gmail.com>
2015-01-11 14:37:33 +0900

    Extract search.indexer module

---
+++ search/indexer.py
@@ -0,0 +1,35 @@
+from django.db import transaction, models
+from work.models import Work, TitleMapping
+from search.models import WorkIndex, WorkTitleIndex, make_key
+
+def add_ranks(objects):
+    objects.sort(key=lambda obj: obj.record_count, reverse=True)
+    rank = 0
+    prev = -1
+    for i, obj in enumerate(objects):
+        if prev != obj.record_count:
+            rank = i + 1
+        prev = obj.record_count
+        obj.rank = rank
+
+@transaction.atomic
+def run():
+    WorkIndex.objects.all().delete()
+
+    objects = []
+    for work in Work.objects.annotate(record_count=models.Count('record')):
+        objects.append(WorkIndex(
+            work_id=work.id,
+            title=work.title,
+            record_count=work.record_count,
+        ))
+    add_ranks(objects)
+    WorkIndex.objects.bulk_create(objects)
+
+    objects = []
+    for mapping in TitleMapping.objects.all():
+        objects.append(WorkTitleIndex(
+            key=make_key(mapping.title),
+            work_id=mapping.work_id,
+        ))
+    WorkTitleIndex.objects.bulk_create(objects)
