c731de036ee0aeec4d86e0f8bc936079fcba4b58
Ditto Kim <dittos@gmail.com>
2015-02-19 00:55:48 +0900

    Cleanup url patterns

--- animeta/urls.py
+++ animeta/urls.py
@@ -6,14 +6,14 @@ import user.views
 urlpatterns = patterns('',
     (r'^$', 'animeta.views.index'),
     (r'^timeline/$', 'animeta.views.redirect_to_index'),
-    (r'^login/', 'user.views.login'),
-    (r'^logout/', 'django.contrib.auth.views.logout', {'next_page': '/'}),
-    (r'^signup/', 'user.views.signup'),
-    (r'^settings/', login_required(user.views.SettingsView.as_view())),
-    (r'^support/', TemplateView.as_view(template_name='support.html')),
+    (r'^login/$', 'user.views.login'),
+    (r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
+    (r'^signup/$', 'user.views.signup'),
+    (r'^settings/$', login_required(user.views.SettingsView.as_view())),
+    (r'^support/$', TemplateView.as_view(template_name='support.html')),
     (r'^api/', include('api.urls')),
 
-    (r'^library/', login_required(user.views.library)),
+    (r'^library/$', login_required(user.views.library)),
     (r'^users/(?P<username>[\w.@+-]+)/', include('user.urls')),
     (r'^records/', include('record.urls')),
     (r'^works/', include('work.urls')),
@@ -21,7 +21,6 @@ urlpatterns = patterns('',
     (r'^connect/', include('connect.urls')),
     (r'^moderation/', include('moderation.urls')),
 
-    (r'^titles/(?P<remainder>.*)/$', 'work.views.old_url'), # compat
     url(r'^-(?P<id>[0-9]+)/?$', 'work.views.post_detail', name='history-detail'),
     (r'^(?P<username>[\w.@+-]+)$', 'user.views.shortcut'),
 
