2d1eefcdf5dfe4a9683b8ce696dcee052fa94d5c
Taeho Kim <dittos@gmail.com>
2015-07-28 23:28:22 +0900

    Update fabfile

--- fabfile.py
+++ fabfile.py
@@ -5,7 +5,6 @@ def deploy():
     with cd('/home/ditto/animeta'):
         run('git pull') # To get password prompt first
         local('rm -f animeta/static/build/*')
-        local('git checkout animeta/static/build/common.css')
         local('NODE_ENV=production ./node_modules/.bin/webpack')
         run('npm install')
         run('env/bin/pip install -r ./requirements.txt')
@@ -13,3 +12,4 @@ def deploy():
         put('animeta/static/build/*', 'animeta/static/build/')
         put('animeta/assets.py', 'animeta/')
         sudo('reload animeta')
+        sudo('reload animeta-node')
