2c2b6b7d4f5c511fcb73ecc23d3a4cd3ec5bb962
Taeho Kim <dittos@gmail.com>
2015-10-24 23:47:19 +0900

    Use simplejson in most path

--- animeta/templatetags/escapejson.py
+++ animeta/templatetags/escapejson.py
@@ -1,4 +1,4 @@
-import json
+import simplejson as json
 from django import template
 from django.utils.safestring import mark_safe
 
--- api/v2/__init__.py
+++ api/v2/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-import json
+import simplejson as json
 from django.http import HttpResponse
 from django.views.generic import View
 
