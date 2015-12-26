fcf96091ef916d3f253ec192c62588ea4948f613
Taeho Kim <dittos@gmail.com>
2015-12-27 00:29:22 +0900

    [fab] deploy_api

--- fabfile.py
+++ fabfile.py
@@ -13,3 +13,11 @@ def deploy():
         put('frontend/assets.json', 'frontend/')
         run('touch ~/uwsgi-services/animeta.ini')
         run('pm2 gracefulReload animeta')
+
+def deploy_api():
+    local('git push')
+    with cd('/home/ditto/animeta'):
+        run('git pull') # To get password prompt first
+        run('env/bin/pip install -r ./requirements.txt')
+        run('env/bin/python manage.py migrate')
+        run('touch ~/uwsgi-services/animeta.ini')
