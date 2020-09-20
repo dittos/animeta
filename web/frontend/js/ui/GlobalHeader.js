04441f0d248df0afd5f76ccdb8478deb30d48ec1
Taeho Kim <dittos@gmail.com>
2020-09-20 11:49:12 +0900

    Fix typo

--- web/frontend/js/ui/GlobalHeader.js
+++ web/frontend/js/ui/GlobalHeader.js
@@ -117,7 +117,7 @@ export class GlobalHeader extends React.Component {
                 <span className={Styles.globalMenuItemText}>작품 찾기</span>
                 {showNotice && (
                   <span className={Styles.globalMenuItemPopover}>
-                    7월 신작 업데이트
+                    10월 신작 업데이트
                   </span>
                 )}
               </Link>
