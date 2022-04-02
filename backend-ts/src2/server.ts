ac4c34c9b5f35159d6369e4d734a6e2ce40d6fc6
Taeho Kim <dittos@gmail.com>
2022-04-02 15:49:49 +0900

    backend-ts: support loading schema from file

--- backend-ts/src2/server.ts
+++ backend-ts/src2/server.ts
@@ -1,4 +1,4 @@
-import fastify, { FastifyInstance } from 'fastify'
+import fastify, { FastifyInstance, FastifySchema } from 'fastify'
 import * as path from 'path'
 import * as fs from 'fs'
 import { createConnection } from 'typeorm'
@@ -20,6 +20,18 @@ const server = fastify({
 
 const middlewareFilename = '_middleware.js'
 
+function loadSchema(endpointPath: string): FastifySchema | null {
+  try {
+    const content = fs.readFileSync(endpointPath.replace(/\.js$/, '.schema.json'), {encoding: 'utf-8'})
+    return JSON.parse(content)
+  } catch (e) {
+    if ((e as NodeJS.ErrnoException).code === 'ENOENT') {
+      return null
+    }
+    throw e
+  }
+}
+
 function registerEndpoints(parent: FastifyInstance, endpointsDir: string, prefix: string) {
   parent.register(async (child, opts) => {
     const files = fs.readdirSync(endpointsDir, { withFileTypes: true })
@@ -33,18 +45,24 @@ function registerEndpoints(parent: FastifyInstance, endpointsDir: string, prefix
       } else if (path.extname(file.name) === '.js') {
         const endpoint = require(fullpath).default as Endpoint | undefined
         if (!endpoint) continue
-        child.route({
-          method: 'POST',
-          url: file.name === 'index.js' ? '/' : '/' + file.name.replace(/\.js$/, ''),
-          schema: {
+        let schema = loadSchema(fullpath)
+        if (!schema && endpoint.Params && endpoint.Result) {
+          // typebox compatibility
+          schema = {
             body: endpoint.Params,
             response: {
               200: endpoint.Result,
             }
-          },
+          }
+        }
+        const handler = endpoint.handler ?? endpoint
+        child.route({
+          method: 'POST',
+          url: file.name === 'index.js' ? '/' : '/' + file.name.replace(/\.js$/, ''),
+          schema: schema ?? undefined,
           handler: async (request, reply) => {
             try {
-              return await endpoint.handler(request.body)
+              return await handler(request.body)
             } catch (e) {
               if (e instanceof HttpException) {
                 reply.status(e.getStatus()).send(e.getResponse())
@@ -61,6 +79,7 @@ function registerEndpoints(parent: FastifyInstance, endpointsDir: string, prefix
   })
 }
 
+server.addSchema(JSON.parse(fs.readFileSync(path.resolve(__dirname, '../gen/schemas/common.json'), {encoding: 'utf8'})))
 registerEndpoints(server, path.join(__dirname, 'endpoints'), '/api')
 
 export async function bootstrap2() {
