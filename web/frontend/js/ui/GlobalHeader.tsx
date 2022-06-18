4b55d1e72c57c651635f52ce4b14cf72f6a45a10
Taeho Kim <dittos@gmail.com>
2022-06-18 12:34:10 +0900

    2022Q3

--- web/frontend/js/ui/GlobalHeader.tsx
+++ web/frontend/js/ui/GlobalHeader.tsx
@@ -66,7 +66,7 @@ export type GlobalHeaderProps = {
 
 export class GlobalHeader extends React.Component<GlobalHeaderProps> {
   static LAST_NOTICE_CLICKED = 'lastNoticeClicked';
-  static noticeId = '2022Q2';
+  static noticeId = '2022Q3';
 
   state = {
     showNotice: false,
@@ -85,8 +85,8 @@ export class GlobalHeader extends React.Component<GlobalHeaderProps> {
   render() {
     const activeMenu = this.props.activeMenu;
     const user = this.props.currentUser;
-    // const showNotice = this.state.showNotice && activeMenu !== 'search' && !this.props.noNotice;
-    const showNotice = false;
+    const showNotice = this.state.showNotice && activeMenu !== 'search' && !this.props.noNotice;
+    // const showNotice = false;
     return (
       <div className={Styles.container}>
         <Layout.CenteredFullWidth className={Styles.header}>
@@ -123,7 +123,7 @@ export class GlobalHeader extends React.Component<GlobalHeaderProps> {
                 <span className={Styles.globalMenuItemText}>작품 찾기</span>
                 {showNotice && (
                   <span className={Styles.globalMenuItemPopover}>
-                    4월 신작 업데이트
+                    7월 신작 업데이트
                   </span>
                 )}
               </Link>
