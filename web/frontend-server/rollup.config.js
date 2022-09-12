a1c14e762c53320324bc0cf72b9017ac83a372cb
Taeho Kim <dittos@gmail.com>
2022-09-12 15:48:01 +0900

    web: frontend-server rollup

---
+++ web/frontend-server/rollup.config.js
@@ -0,0 +1,39 @@
+// import typescript from '@rollup/plugin-typescript';
+import json from '@rollup/plugin-json';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+
+const commonOptions = {
+  plugins: [
+    // typescript(),
+    json(),
+    commonjs({
+      extensions: ['.js', '.ts'],
+      // Some code in bundle.server.js require querystring module dynamically
+      ignoreDynamicRequires: true,
+    }),
+    resolve(),
+  ],
+  external: [
+    'util',
+  ],
+}
+
+export default [
+  {
+    input: 'dist/frontendServer.js',
+    output: {
+      file: 'dist/bundle.js',
+      format: 'cjs',
+    },
+    ...commonOptions,
+  },
+  {
+    input: 'dist/lambdaHandler.js',
+    output: {
+      file: 'dist/bundle-lambda.js',
+      format: 'cjs',
+    },
+    ...commonOptions,
+  },
+]
