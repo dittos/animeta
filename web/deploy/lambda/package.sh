181aa857fc3b0da7193b2b4d2166b30d95710182
Taeho Kim <dittos@gmail.com>
2022-09-16 23:55:42 +0900

    fix dir

--- web/deploy/lambda/package.sh
+++ web/deploy/lambda/package.sh
@@ -1,12 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
 rm -rf deploy/lambda/dist
-mkdir -p deploy/lambda/dist/app
+mkdir -p deploy/lambda/dist/app/{frontend-server,frontend}
 
-cp -r shared deploy/lambda/dist/app/shared
-cp -r frontend-server/dist deploy/lambda/dist/app/frontend-server/dist
-cp -r frontend/dist deploy/lambda/dist/app/frontend/dist
+cp -r frontend-server/dist deploy/lambda/dist/app/frontend-server/
+cp -r frontend/dist deploy/lambda/dist/app/frontend/
 
 echo ANIMETA_FRONTEND_DIST_PATH=app/frontend/dist >> deploy/lambda/dist/.env
 
