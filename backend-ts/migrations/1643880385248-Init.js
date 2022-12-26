427137ca6b86294dd5e98c0d06264b34003be5b4
Taeho Kim <dittos@gmail.com>
2022-12-26 22:40:30 +0900

    backend-ts: temporary disable init migration

--- backend-ts/migrations/1643880385248-Init.js
+++ backend-ts/migrations/1643880385248-Init.js
@@ -4,7 +4,7 @@ const path = require('path')
 module.exports = class Init1643880385248 {
 
     async up(queryRunner) {
-      await queryRunner.query(fs.readFileSync(path.resolve(__dirname, '../schema.sql'), {encoding: 'utf-8'}));
+      // await queryRunner.query(fs.readFileSync(path.resolve(__dirname, '../schema.sql'), {encoding: 'utf-8'}));
     }
 
     async down(queryRunner) {
