762d3b1a875e3175198de17b1761258e77706a69
Taeho Kim <dittos@gmail.com>
2023-03-12 22:24:04 +0900

    web(admin): add namuwiki parsing pattern

--- web/frontend/js/admin/WorkAddForm.tsx
+++ web/frontend/js/admin/WorkAddForm.tsx
@@ -21,7 +21,7 @@ export const WorkAddForm: React.FC<{}> = () => {
       const anchor = decodeURIComponent(url.hash.substring(1))
       const namuRef = anchor ? `${page}#${anchor}` : page
       
-      const title = page.replace(/\/애니메이션$/, '')
+      const title = page.replace(/\/애니메이션$/, '').replace(/\(애니메이션( [0-9]+기)?\)/, '').trim()
       setNamuRef(namuRef)
       ref.current!.setValue(title)
     }
