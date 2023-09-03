961b823462e85490837f5fe3d28e6270d346e2ff
Taeho Kim <dittos@gmail.com>
2023-09-03 11:41:57 +0900

    web: extract common ErrorBoundary

--- web/frontend/js/layouts.tsx
+++ web/frontend/js/layouts.tsx
@@ -3,31 +3,20 @@ import React from 'react';
 import { UserDTO } from '../../shared/types';
 import { GlobalHeader, GlobalHeaderProps } from './ui/GlobalHeader';
 import UserLayout, { UserLayoutProps, UserLayoutPropsData } from './ui/UserLayout';
-import * as Sentry from '@sentry/react';
-
-function ErrorFallback() {
-  return <>
-    <h1>오류가 발생했습니다.</h1>
-    <p>페이지 새로 고침 후 다시 시도해보세요.</p>
-    <p>
-      발생한 오류는 관리자에게 전송되었으며, 문제가 계속 발생한다면{' '}
-      <a href="/support/">버그 신고</a> 부탁드립니다.
-    </p>
-  </>
-}
+import { ErrorBoundary } from './layouts/ErrorBoundary';
 
 export function App<Props extends { data: { currentUser: UserDTO | null } }>(
   Component: React.JSXElementConstructor<Props>,
   globalHeaderProps?: Partial<GlobalHeaderProps>
 ) {
   return (props: Props) => (
-    <Sentry.ErrorBoundary fallback={<ErrorFallback />}>
+    <ErrorBoundary>
       <GlobalHeader
         currentUsername={props.data.currentUser?.name}
         {...globalHeaderProps}
       />
       <Component {...props} />
-    </Sentry.ErrorBoundary>
+    </ErrorBoundary>
   );
 }
 
@@ -37,7 +26,7 @@ export function User<Data extends UserLayoutPropsData>(
   globalHeaderProps: Partial<GlobalHeaderProps> = {}
 ) {
   return (props: RouteComponentProps<Data>) => (
-    <Sentry.ErrorBoundary fallback={<ErrorFallback />}>
+    <ErrorBoundary>
       <GlobalHeader
         currentUsername={props.data.currentUser?.name}
         activeMenu={
@@ -50,7 +39,7 @@ export function User<Data extends UserLayoutPropsData>(
       <UserLayout {...props} {...layoutProps}>
         <Component {...props} />
       </UserLayout>
-    </Sentry.ErrorBoundary>
+    </ErrorBoundary>
   );
 }
 
