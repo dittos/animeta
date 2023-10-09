caa5d1ca474a1d75c804cdfec76831e333d335d0
Taeho Kim <dittos@gmail.com>
2023-10-09 15:45:01 +0900

    backend-ts: remove nest.js controllers

--- backend-ts/src/main.ts
+++ backend-ts/src/main.ts
@@ -1,21 +1,5 @@
-import { NestFactory } from '@nestjs/core';
-import { AppProdModule } from './app.module';
-import * as Sentry from '@sentry/node';
-import { ConfigService } from '@nestjs/config';
-import * as cookieParser from 'cookie-parser';
-import { SentryInterceptor } from './sentry.interceptor';
-import { bootstrap2 } from 'src2/server';
+import * as dotenv from 'dotenv'
+dotenv.config()
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppProdModule);
-  const sentryDsn = app.get(ConfigService).get('SENTRY_DSN');
-  if (sentryDsn) {
-    Sentry.init({ dsn: sentryDsn });
-  }
-  app.useGlobalInterceptors(new SentryInterceptor());
-  app.use(cookieParser());
-  app.enableShutdownHooks();
-  await app.listen(8081);
-}
-bootstrap();
-bootstrap2();
+import { bootstrap2 } from 'src2/server'
+bootstrap2()
