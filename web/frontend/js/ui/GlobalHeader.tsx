0882e3d2f56fe514dc843fe92d3f9cbd40212ea2
Taeho Kim <dittos@gmail.com>
2025-03-15 11:09:51 +0900

    2025Q2

--- web/frontend/js/ui/GlobalHeader.tsx
+++ web/frontend/js/ui/GlobalHeader.tsx
@@ -67,8 +67,8 @@ export type GlobalHeaderProps = {
 
 export class GlobalHeader extends React.Component<GlobalHeaderProps> {
   static LAST_NOTICE_CLICKED = 'lastNoticeClicked';
-  static noticeId = '2025Q1';
-  static showNoticeUntil = new Date("2025-01-15T00:00:00+09:00");
+  static noticeId = '2025Q2';
+  static showNoticeUntil = new Date("2025-04-15T00:00:00+09:00");
 
   state = {
     showNotice: false,
@@ -125,7 +125,7 @@ export class GlobalHeader extends React.Component<GlobalHeaderProps> {
                 <span className={Styles.globalMenuItemText}>작품 찾기</span>
                 {showNotice && (
                   <span className={Styles.globalMenuItemPopover}>
-                    2025년 1월 신작 업데이트!
+                    4월 신작 업데이트!
                   </span>
                 )}
               </Link>
